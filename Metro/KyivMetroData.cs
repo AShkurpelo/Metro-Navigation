2100b900dcacfd944bcb5bcbc3691a0d7f35c10b
AShkurpelo <sania575@gmail.com>
2017-05-09 15:41:20 +0300

    Add files via upload

---
+++ Metro/KyivMetroData.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Shapes;
+using System.Globalization;
+using System.Resources;
+using System.Windows.Media;
+
+namespace Metro
+{
+    class KyivMetroData
+    {
+        public struct station
+        {
+            public Ellipse ellipse;
+            public int line;
+        }
+
+        //Dictionary to hold all stations by their id
+        public Dictionary<int, station> stations = new Dictionary<int, station>();
+        //Dictionary to hold pair of lines and pair of stations that connect them
+        public Dictionary<Tuple<int, int>, Tuple<int, int>> transfers = new Dictionary<Tuple<int, int>, Tuple<int, int>>();
+
+        public KyivMetroData()
+        {           
+            ResourceSet resourceSet = Properties.Resources.ResourceManager.GetResourceSet(CultureInfo.CurrentCulture, true, true);
+
+            //Reading all stations possitions on a map and connections between them
+            station temp;
+            try
+            {
+                foreach (DictionaryEntry entry in resourceSet)
+                {
+                    if (entry.Value is string && entry.Key.ToString().Substring(0, 2) == "Id")
+                    {
+                        //each station have its line and in line there are coordinates on a map, lineId and stationId
+                        string[] buff = entry.Value.ToString().Split();
+
+                        temp = new station();
+                        temp.ellipse = new Ellipse();
+                        temp.ellipse.Margin = new Thickness(int.Parse(buff[0]), int.Parse(buff[1]), 0, 0);
+                        temp.line = int.Parse(buff[2]);
+                        temp.ellipse.Width = 18;
+                        temp.ellipse.Height = 18;
+                        temp.ellipse.Fill = new SolidColorBrush(Color.FromRgb(255, 255, 255));
+
+                        int id = int.Parse(buff[3]);
+
+                        stations.Add(id, temp);
+                    }
+                    else if (entry.Value is string && entry.Key.ToString().Substring(0, 4) == "Pair")
+                    {
+                        string[] buff = entry.Value.ToString().Split();
+
+                        Tuple<int, int> lines = new Tuple<int, int>(int.Parse(buff[0]), int.Parse(buff[1]));
+                        Tuple<int, int> stationsIds = new Tuple<int, int>(int.Parse(buff[2]), int.Parse(buff[3]));
+
+                        transfers.Add(lines, stationsIds);
+                    }
+                }
+            }
+            catch { MessageBox.Show("Something went wrong."); }
+        }
+
+        public List<Ellipse> FindPath(Ellipse from, Ellipse to)
+        {
+            List<Ellipse> res = new List<Ellipse>();
+
+            int startId = stations.First(a => a.Value.ellipse == from).Key;
+            int goalId = stations.First(a => a.Value.ellipse == to).Key;
+
+            int curr = startId;
+            int tempGoal = goalId;
+            bool withTransf = false;
+            int transfToId = 0;
+
+            while (curr != goalId)
+            {
+                withTransf = false;
+                if (stations[curr].line != stations[tempGoal].line)
+                {
+                    //if stations on different lines, program search pair that connect these lines and go throw this 
+                    //tranfer to destination station. More difficult search algorithms don't nessesery for Kyiv metropoliten.
+                    withTransf = true;
+                    int line1 = Math.Min(stations[curr].line, stations[tempGoal].line);
+                    int line2 = Math.Max(stations[curr].line, stations[tempGoal].line);
+
+                    Tuple<int, int> lines = new Tuple<int, int>(line1, line2);
+
+                    var transf = transfers[lines];
+                    if (stations[transf.Item1].line == stations[curr].line)
+                    {
+                        tempGoal = transf.Item1;
+                        transfToId = transf.Item2;
+                    }
+                    else
+                    {
+                        tempGoal = transf.Item2;
+                        transfToId = transf.Item1;
+                    }
+                }
+
+                for (int i = curr; i != tempGoal; i += (curr < tempGoal) ? 1 : -1)
+                {
+                    res.Add(stations[i].ellipse);
+                }
+                res.Add(stations[tempGoal].ellipse);
+
+                curr = (withTransf) ? transfToId : tempGoal;
+                tempGoal = goalId;
+                if (curr == tempGoal && withTransf)
+                    res.Add(stations[curr].ellipse);
+            }
+
+            return res;
+        }
+
+    }
+}
---
+++ Metro/MainWindow.xaml.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Media.Animation;
+
+namespace Metro
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        KyivMetroData metro = new KyivMetroData();
+
+        List<Ellipse> selected = new List<Ellipse>();
+        List<Ellipse> path = new List<Ellipse>();
+        int selectedCount = 0;
+
+        SolidColorBrush idleColorBrush = new SolidColorBrush(Color.FromRgb(255, 255, 255));
+        SolidColorBrush focusedColorBrush = new SolidColorBrush(Color.FromRgb(255, 216, 170));
+        SolidColorBrush selectedColorBrush = new SolidColorBrush(Color.FromRgb(255, 216, 0));
+        SolidColorBrush pathColorBrush = new SolidColorBrush(Color.FromRgb(255, 216, 0));
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            Canvas.Focus();
+
+            //Adding events to all stations ellipses and adding them to canvas
+            foreach (var el in metro.stations)
+            {
+                el.Value.ellipse.MouseEnter += StationMouseEnter;
+                el.Value.ellipse.MouseLeave += StationMouseLeave;
+                el.Value.ellipse.MouseLeftButtonUp += StationMouseClick;
+                Canvas.Children.Add(el.Value.ellipse);
+            }
+
+            //Checking "Dont show anymore" property of entry help
+            if (Properties.Settings.Default.ShowHelp == "True")
+            {
+                Properties.Settings.Default.ShowHelp = (!StartHelpShow()).ToString();
+                Properties.Settings.Default.Save();
+            }
+                                    
+        }
+
+        //Making own MessegeBox with checkbox "Dont show anymore"
+        private bool StartHelpShow()
+        {
+            Window helpWindow = new Window();
+            helpWindow.Width = 460;
+            helpWindow.Height = 200;
+            helpWindow.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            helpWindow.Title = "Довідка";
+
+            StackPanel panel = new StackPanel() { Orientation = Orientation.Vertical };
+
+            Label text = new Label();
+            text.Content = "Натисніть ліву клавішу миші на двох станціях, які хочете обрати.\n\nНатисніть праву клавішу миші для скасування вибору.";
+            text.Margin = new Thickness(10, 10, 0, 0);
+
+            CheckBox chk = new CheckBox();
+            chk.Content = "Не показувати знову";
+            chk.Margin = new Thickness(15, 20, 0, 0);
+
+            Button ok = new Button();
+            ok.Content = "OK";
+            ok.Height = 30;
+            ok.Width = 50;
+            ok.Margin = new Thickness(30, 15, 0, 0);
+            ok.Click += (sender, e) => { helpWindow.Close(); };
+
+            panel.Children.Add(text);
+            panel.Children.Add(chk);
+            panel.Children.Add(ok);
+            helpWindow.Content = panel;
+
+            helpWindow.ShowDialog();
+            return (bool)chk.IsChecked;
+        }
+
+        private void MenuHelp_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Натисніть ліву клавішу миші на двох станціях, які хочете обрати.\nНатисніть праву клавішу миші для скасування вибору.");
+        }
+
+        private void MenuExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void StationMouseEnter(object sender, MouseEventArgs e)
+        {
+            var obj = (Ellipse)sender;
+
+            //changing station color then mouse enter it
+            if (!selected.Contains(obj) && !path.Contains(obj))
+            {
+                obj.Fill = focusedColorBrush;
+            }
+        }
+
+        private void StationMouseLeave(object sender, MouseEventArgs e)
+        {
+            var obj = (Ellipse)sender;
+
+            //changing station color to default then mouse leave it
+            if (!selected.Contains(obj) && !path.Contains(obj))
+            {
+                obj.Fill = idleColorBrush;
+            }
+        }
+
+        private void StationMouseClick(object sender, MouseButtonEventArgs e)
+        {
+            var obj = (Ellipse)sender;
+
+            //selecting stations
+            if (selectedCount < 2 && !selected.Contains(obj))
+            {
+                selected.Add(obj);
+                selectedCount++;
+                obj.Fill = selectedColorBrush;
+                
+                if (selectedCount == 2)
+                {
+                    path = metro.FindPath(selected[0], selected[1]);
+                    DrawPath();
+                }
+            }
+        }
+
+        private void DrawPath()
+        {
+            ColorAnimation animation;
+            Storyboard storyboard;
+            foreach (var el in path)
+            {
+                el.Fill = pathColorBrush;
+
+                //Animation block
+                animation = new ColorAnimation(Color.FromRgb(255, 216, 0), Color.FromRgb(255, 106, 0), 
+                    new Duration(TimeSpan.FromSeconds(0.8)));
+                animation.RepeatBehavior = RepeatBehavior.Forever;
+                animation.AutoReverse = true;
+                storyboard = new Storyboard();
+                Storyboard.SetTargetProperty(animation, new PropertyPath("(Ellipse.Fill).(SolidColorBrush.Color)") );
+                storyboard.Children.Add(animation);
+                
+                el.BeginStoryboard(storyboard);
+            }
+        }
+
+        private void UndrawPath()
+        {
+            foreach (var el in path)
+            {
+                el.Fill = idleColorBrush;
+            }
+        }
+
+        private void Canvas_MouseRightButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            foreach (var el in selected)
+            {
+                ((Ellipse)Canvas.Children[Canvas.Children.IndexOf(el)]).Fill = new SolidColorBrush(Color.FromRgb(255, 255, 255));
+            }
+            UndrawPath();
+            selected.Clear();
+            path.Clear();
+            selectedCount = 0;
+        }
+    }
+}
